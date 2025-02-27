3390b4a553e9053dc4a9d2aafbcb7d095573502d
pithecantrope <fartdraft@gmail.com>
2025-02-27 20:00:57 +0300

    +arena_alloc test; -LABEL, ASSERT, SAN flags

--- src/tests.h
+++ src/tests.h
@@ -1,4 +1,17 @@
 #ifdef TESTS
+arena a = {0};
+
+TEST("arena_alloc") {
+        arena_alloc(&a, bool);
+        arena_alloc(&a, s8, 0);
+        EXPECT(a.beg == a.end && a.beg->used == sizeof(usize));
+        arena_alloc_aligned(&a, 8 + ARENA_REGION_CAPACITY, alignof(uintptr_t), 1);
+        EXPECT(a.beg != a.end);
+        usize* pusize = arena_alloc(&a, usize);
+        EXPECT(a.beg->used == 2 * sizeof(usize));
+        *pusize = 19;
+}
+
 const s8* empty = s8("");
 const s8* hello = s8("Hello World!");
 
@@ -33,6 +46,8 @@ TEST("s8count") {
         EXPECT(12 == s8count(hello, empty));
         EXPECT(3 == s8count(hello, s8("l")));
 }
+
+arena_free(&a);
 #else
 // IWYU pragma: begin_keep
 #include "core.h"
