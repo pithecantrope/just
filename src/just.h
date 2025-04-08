cb6c0c67551fa39d9532c7fd8a72bc6bc12dcc86
pithecantrope <fartdraft@gmail.com>
2025-04-08 12:15:56 +0300

    First align then size

--- src/just.h
+++ src/just.h
@@ -5,18 +5,14 @@
 #ifndef JUST_H
 #define JUST_H
 
-#ifndef JUST_ASSERT
 #include <assert.h>
-#define JUST_ASSERT assert
-#endif // JUST_ASSERT
-#include <float.h>
 #include <stdalign.h>
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <stdio.h>  // printf
+#include <stdlib.h> // malloc/free
+#include <string.h> // memXXX
 
 // Types -------------------------------------------------------------------------------------------
 typedef char      byte;
@@ -28,17 +24,12 @@ typedef int32_t   i32;
 typedef uint32_t  u32;
 typedef int64_t   i64;
 typedef uint64_t  u64;
-typedef float     f32;
-#define F32_MIN   FLT_MIN
-#define F32_MAX   FLT_MAX
-typedef double    f64;
-#define F64_MIN   DBL_MIN
-#define F64_MAX   DBL_MAX
 typedef ptrdiff_t isize;
 #define ISIZE_MIN PTRDIFF_MIN
 #define ISIZE_MAX PTRDIFF_MAX
 typedef size_t    usize;
-#define USIZE_MAX SIZE_MAX
+typedef float     f32;
+typedef double    f64;
 
 typedef struct {
         usize used, cap;
@@ -49,32 +40,20 @@ typedef struct {
         usize used;
 } arena_savepoint;
 
-typedef struct {
-        u8* data;
-        isize len;
-} s8;
-typedef struct {
-        s8* data;
-        isize len;
-} s8s;
-typedef struct {
-        isize* data;
-        isize len;
-} isizes;
-
 // General -----------------------------------------------------------------------------------------
+#define INLINE             static inline
 #define MIN(a, b)          ((a) < (b) ? (a) : (b))
 #define MAX(a, b)          ((a) > (b) ? (a) : (b))
 #define DIFF(a, b)         ((a) > (b) ? (a) - (b) : (b) - (a))
 #define ABS(x)             (((x) > 0) ? (x) : -(x))
 #define IS_POW2(x)         (((x) > 0) && (((x) & ((x) - 1)) == 0))
 #define IS_IN(min, x, max) ((min) <= (x) && (x) <= (max))
-#define INLINE             static inline
+// Suppress up to 8 arguments
 #define TODO(...)          (JUST_TODO_1(__VA_ARGS__, 0))
 
 // Test  -------------------------------------------------------------------------------------------
 #define TEST(name) TEST = name;
-#define EXPECT(condition) if (!(condition)) printf("FAIL: %s:%d: Test `%s':\n\tCondition: `%s'\n", __FILE__, __LINE__, TEST, #condition)
+#define EXPECT(condition) if (!(condition)) printf("FAIL: %s:%d: Test '%s':\n\tCondition: '%s'\n", __FILE__, __LINE__, TEST, #condition)
 #if 0   // Example:
 // tests.h:
         #ifdef TESTS
@@ -126,65 +105,23 @@ INLINE u8   u8swapcase (u8 c) { return u8is_upper(c) ? u8lower(c) : (u8is_lower(
 arena* arena_create(usize capacity);
 INLINE void arena_reset  (arena* a) { a->used = 0; }
 INLINE void arena_destroy(arena* a) { free(a); }
-void*   arena_alloc(arena* a, usize size, usize align, usize count);
+void*   arena_alloc(arena* a, usize align, usize size, usize count);
 #define alloc(...) JUST_ALLOCX(__VA_ARGS__, JUST_ALLOC3, JUST_ALLOC2, 0)(__VA_ARGS__)
 INLINE arena_savepoint arena_save(arena* a) { return (arena_savepoint){.arena = a, .used = a->used}; }
 INLINE void arena_restore(arena_savepoint save) { save.arena->used = save.used; }
 
-// s8 ----------------------------------------------------------------------------------------------
-#define s8(s) (s8){.data = (u8*)(s), .len = (isize)(sizeof(s) - 1)}
-#define S8(a, s) s8new(a, s, sizeof(s) - 1)
-s8 s8new   (arena* a, const char* data, usize len);
-s8 s8dup   (arena* a, s8 s);
-s8 s8slice (arena* a, s8 s, isize start, isize stop, isize step);
-s8 s8repeat(arena* a, s8 s, isize n);
-s8 s8cat   (arena* a, s8 s1, s8 s2);
-s8 s8inject(arena* a, s8 s1, isize index, isize len, s8 s2);
-
-i32 s8cmp (s8 s1, s8 s2);
-i32 s8icmp(s8 s1, s8 s2);
-INLINE bool s8eq (s8 s1, s8 s2) { return s8cmp (s1, s2) == 0; }
-INLINE bool s8ieq(s8 s1, s8 s2) { return s8icmp(s1, s2) == 0; }
-INLINE bool s8starts_with(s8 s, s8 prefix) { return (s.len >= prefix.len) && memcmp(s.data, prefix.data, (usize)prefix.len) == 0; }
-INLINE bool s8ends_with  (s8 s, s8 suffix) { return (s.len >= suffix.len) && memcmp(s.data + (s.len - suffix.len), suffix.data, (usize)suffix.len) == 0; }
-
-s8 s8span(s8 s, isize index, isize len);
-isize s8find (s8 s, s8 sub);
-isize s8count(s8 s, s8 sub);
-isizes s8findall(arena* a, s8 s, s8 sub);
-s8s s8split(arena* a, s8 s, s8 sep);
-
-INLINE bool s8is_digit  (s8 s) { for (isize i = 0; i < s.len; ++i) { if (!u8is_digit (s.data[i])) return false; } return true; }
-INLINE bool s8is_upper  (s8 s) { for (isize i = 0; i < s.len; ++i) { if (!u8is_upper (s.data[i])) return false; } return true; }
-INLINE bool s8is_lower  (s8 s) { for (isize i = 0; i < s.len; ++i) { if (!u8is_lower (s.data[i])) return false; } return true; }
-INLINE bool s8is_print  (s8 s) { for (isize i = 0; i < s.len; ++i) { if (!u8is_print (s.data[i])) return false; } return true; }
-INLINE bool s8is_graph  (s8 s) { for (isize i = 0; i < s.len; ++i) { if (!u8is_graph (s.data[i])) return false; } return true; }
-INLINE bool s8is_alpha  (s8 s) { for (isize i = 0; i < s.len; ++i) { if (!u8is_alpha (s.data[i])) return false; } return true; }
-INLINE bool s8is_alnum  (s8 s) { for (isize i = 0; i < s.len; ++i) { if (!u8is_alnum (s.data[i])) return false; } return true; }
-INLINE bool s8is_xdigit (s8 s) { for (isize i = 0; i < s.len; ++i) { if (!u8is_xdigit(s.data[i])) return false; } return true; }
-INLINE bool s8is_blank  (s8 s) { for (isize i = 0; i < s.len; ++i) { if (!u8is_blank (s.data[i])) return false; } return true; }
-INLINE bool s8is_space  (s8 s) { for (isize i = 0; i < s.len; ++i) { if (!u8is_space (s.data[i])) return false; } return true; }
-INLINE bool s8is_ascii  (s8 s) { for (isize i = 0; i < s.len; ++i) { if (!u8is_ascii (s.data[i])) return false; } return true; }
-INLINE bool s8is_cntrl  (s8 s) { for (isize i = 0; i < s.len; ++i) { if (!u8is_cntrl (s.data[i])) return false; } return true; }
-INLINE bool s8is_punct  (s8 s) { for (isize i = 0; i < s.len; ++i) { if (!u8is_punct (s.data[i])) return false; } return true; }
-       bool s8is_title  (s8 s);
-INLINE s8   s8upper     (s8 s) { for (isize i = 0; i < s.len; ++i) { s.data[i] = u8upper   (s.data[i]); } return s; }
-INLINE s8   s8lower     (s8 s) { for (isize i = 0; i < s.len; ++i) { s.data[i] = u8lower   (s.data[i]); } return s; }
-INLINE s8   s8swapcase  (s8 s) { for (isize i = 0; i < s.len; ++i) { s.data[i] = u8swapcase(s.data[i]); } return s; }
-       s8   s8capitalize(s8 s);
-       s8   s8title     (s8 s);
 // Internals ---------------------------------------------------------------------------------------
-#define JUST_TODO_1(arg, ...)              (void)arg, JUST_TODO_2(__VA_ARGS__, 0)
-#define JUST_TODO_2(arg, ...)              (void)arg, JUST_TODO_3(__VA_ARGS__, 0)
-#define JUST_TODO_3(arg, ...)              (void)arg, JUST_TODO_4(__VA_ARGS__, 0)
-#define JUST_TODO_4(arg, ...)              (void)arg, JUST_TODO_5(__VA_ARGS__, 0)
-#define JUST_TODO_5(arg, ...)              (void)arg, JUST_TODO_6(__VA_ARGS__, 0)
-#define JUST_TODO_6(arg, ...)              (void)arg, JUST_TODO_7(__VA_ARGS__, 0)
-#define JUST_TODO_7(arg, ...)              (void)arg, JUST_TODO_8(__VA_ARGS__, 0)
-#define JUST_TODO_8(arg, ...)              (void)arg
+#define JUST_TODO_1(arg, ...) (void)arg, JUST_TODO_2(__VA_ARGS__, 0)
+#define JUST_TODO_2(arg, ...) (void)arg, JUST_TODO_3(__VA_ARGS__, 0)
+#define JUST_TODO_3(arg, ...) (void)arg, JUST_TODO_4(__VA_ARGS__, 0)
+#define JUST_TODO_4(arg, ...) (void)arg, JUST_TODO_5(__VA_ARGS__, 0)
+#define JUST_TODO_5(arg, ...) (void)arg, JUST_TODO_6(__VA_ARGS__, 0)
+#define JUST_TODO_6(arg, ...) (void)arg, JUST_TODO_7(__VA_ARGS__, 0)
+#define JUST_TODO_7(arg, ...) (void)arg, JUST_TODO_8(__VA_ARGS__, 0)
+#define JUST_TODO_8(arg, ...) (void)arg
 
 #define JUST_ALLOCX(_1, _2, _3, NAME, ...) NAME
-#define JUST_ALLOC2(a, t)                  (t*)arena_alloc(a, sizeof(t), alignof(t), 1)
-#define JUST_ALLOC3(a, t, n)               (t*)arena_alloc(a, sizeof(t), alignof(t), (usize)(n))
+#define JUST_ALLOC2(a, t)                  (t*)arena_alloc(a, alignof(t), sizeof(t), 1)
+#define JUST_ALLOC3(a, t, n)               (t*)arena_alloc(a, alignof(t), sizeof(t), (usize)(n))
 
 #endif // JUST_H
