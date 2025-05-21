5eb933909621e5164b69105871367030802bf23f
pithecantrope <fartdraft@gmail.com>
2025-05-21 19:19:03 +0300

    here we go again

--- src/just.h
+++ src/just.h
@@ -2,29 +2,14 @@
 #ifndef JUST_H
 #define JUST_H
 
-// General -----------------------------------------------------------------------------------------
 #include <assert.h>
-#include <inttypes.h>
 #include <stdalign.h>
 #include <stdbool.h>
-#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-typedef char     byte;
-typedef int8_t   i8;
-typedef uint8_t  u8;
-typedef int16_t  i16;
-typedef uint16_t u16;
-typedef int32_t  i32;
-typedef uint32_t u32;
-typedef int64_t  i64;
-typedef uint64_t u64;
-typedef float    f32;
-typedef double   f64;
-
-#define MIN(a, b)       ((a) < (b) ? (a) : (b))
+#define MIN(a, b)       ((a) > (b) ? (b) : (a))
 #define MAX(a, b)       ((a) > (b) ? (a) : (b))
 #define DIFF(a, b)      ((a) > (b) ? (a) - (b) : (b) - (a))
 #define ABS(x)          (((x) > 0) ? (x) : -(x))
@@ -37,22 +22,22 @@ typedef double   f64;
 
 // arena -------------------------------------------------------------------------------------------
 typedef struct {
-        u64 used, cap;
-        byte data[];
+        size_t used, cap;
+        char data[];
 } arena;
-#define PRI_arena "{used:%" PRIu64 ", cap:%" PRIu64 ", data:%p}"
-#define FMT_arena(arena) (arena)->used, (arena)->cap, (void*)(arena)->data
+#define PRI_arena "{used:%zu, cap:%zu, data:%p}"
+#define FMT_arena(arena) (arena)->used, (arena)->cap, (arena)->data
 typedef struct {
         arena* arena;
-        u64 used;
+        size_t used;
 } arena_savepoint;
 
-arena* arena_create(u64 capacity);
+arena* arena_create(size_t capacity);
 INLINE void arena_reset  (arena* a) { a->used = 0; }
 INLINE void arena_destroy(arena* a) { free(a); }
-void*   arena_alloc(arena* a, u64 align, u64 size, u64 count);
+void*   arena_alloc(arena* a, size_t align, size_t size, size_t count);
 #define JUST_ALLOC2(arena, type)    (type*)arena_alloc(arena, alignof(type), sizeof(type), 1)
-#define JUST_ALLOC3(arena, type, n) (type*)arena_alloc(arena, alignof(type), sizeof(type), (u64)(n))
+#define JUST_ALLOC3(arena, type, n) (type*)arena_alloc(arena, alignof(type), sizeof(type), n)
 #define alloc(...) JUST3X(__VA_ARGS__, JUST_ALLOC3, JUST_ALLOC2, 0, 0)(__VA_ARGS__)
 INLINE arena_savepoint arena_save(arena* a) { return (arena_savepoint){.arena = a, .used = a->used}; }
 INLINE void arena_restore(arena_savepoint save) { save.arena->used = save.used; }
@@ -84,6 +69,10 @@ typedef struct {
 } string;
 #define PRI_string "%.*s"
 #define FMT_string(string) (int)(string).len, (char*)(string).data
+typedef struct {
+        string* data;
+        i32 len;
+} strings;
 
 #define JUST_S1(literal) (string){.data = (ascii*)(literal), .len = (i32)(sizeof(literal) - 1)}
 #define JUST_S2(arena, literal) string_new(arena, literal, sizeof(literal) - 1)
@@ -94,6 +83,7 @@ string string_dup(arena* a, string s);
 string string_cat(arena* a, string self, string s);
 string string_inject(arena* a, string self, i32 index, i32 len, string s);
 
+bool string_in(string self, string s);
 int string_cmp  (string s1, string s2);
 int string_icmp (string s1, string s2);
 INLINE bool string_eq (string s1, string s2) { return s1.len == s2.len && memcmp(s1.data, s2.data, (size_t)s1.len) == 0; }
@@ -101,10 +91,6 @@ INLINE bool string_eq (string s1, string s2) { return s1.len == s2.len && memcmp
 INLINE bool string_startswith(string s, string prefix) { return (s.len >= prefix.len) && memcmp(s.data, prefix.data, (size_t)prefix.len) == 0; }
 INLINE bool string_endswith  (string s, string suffix) { return (s.len >= suffix.len) && memcmp(s.data + (s.len - suffix.len), suffix.data, (size_t)suffix.len) == 0; }
 
-typedef struct {
-        string* data;
-        i32 len;
-} strings;
 string string_view(string s, i32 index, i32 len);
 
 static const string SPACE = {(ascii*)" \t\n\r\f\v", 5};
@@ -115,7 +101,10 @@ static const string LOWER = {(ascii*)"abcdefghijklmnopqrstuvwxyz", 26};
 static const string ALPHA = {(ascii*)"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz", 52};
 static const string WORD  = {(ascii*)"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_", 63};
 static const string PUNCT = {(ascii*)"!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~", 32};
-string string_trim (string s, string chars);
+string string_lstrip(string s, string chars);
+string string_rstrip(string s, string chars);
+string string_strip (string s, string chars);
+
 string string_erase (arena* a, string s, string chars);
 strings string_splitany(arena* a, string s, string chars);
 string string_replaceany(arena* a, string s, string chars);
